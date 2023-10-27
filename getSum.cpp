6090ef2a46cf54c863a88c89b1adc6edebe575da
Meqdad Amr <meqdad_@outlook.com>
2023-10-27 15:10:30 +0200

    add getMin

--- getSum.cpp
+++ getSum.cpp
@@ -11,9 +11,21 @@ int getSum(vector<int> vec) {
     return sum;
 }
 
+int getMin(vector<int> vec) {
+    int min = vec[0];
+    for (int &i : vec) {
+        if (i < min) {
+            min = i;
+        }
+    }
+    return min;
+}
+
 int main() {
     vector<int> myVec = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
     int sum = getSum(myVec);
+    int min = getMin(myVec);
     cout << "The sum of the elements in the vector is: " << sum << endl;
+    cout << "The minimum element in the vector is: " << min << endl;
     return 0;
 }
